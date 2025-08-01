f238d0427e111f2f19b8158914619f955082006c
MatiSancassani <matiassancassani@gmail.com>
2025-08-01 12:45:44 -0300

    first commit

---
+++ app/(curso)/conversor.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function conversor() {
+  return (
+    <View>
+      <Text>conversor</Text>
+    </View>
+  );
+}
